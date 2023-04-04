86817ebd792de748c64fd6007a82890c4c27fefd
MelWal05 <melker.wallebom@skola.taby.se>
2023-04-04 10:14:49 +0200

    Klar med Coinflip

--- app.js
+++ app.js
@@ -1,32 +1,85 @@
-let coinflipContainer = document.getElementById("coinflipContainer");
-let coinflipImage = document.createElement("img");
-let btnRandom = document.getElementById("btnRandom");
+let wallet = document.getElementById("wallet")
+let cashButton = document.getElementById("getCash")
 
-btnRandom.addEventListener("click", function (e) {
-  randomNumber = Math.floor(Math.random() * 2);
-  if (randomNumber == 1) {
-    coinflipImage.src = "pokerred.png";
-    coinflipContainer.append(coinflipImage);
-    coinflipImage.style.width = "200px";
-    coinflipImage.style.height = "200px";
-    coinflipContainer.style.transition = "3s";
-    coinflipContainer.style.transform = "rotate(180deg)";
-  } else {
-    coinflipImage.src = "pokerblack.png ";
-    coinflipContainer.append(coinflipImage);
-    coinflipImage.style.width = "200px";
-    coinflipImage.style.height = "200px";
-    coinflipContainer.style.transition = "3s";
-    coinflipContainer.style.transform = "rotate(180deg)";
+let currencyAmount = 0
+wallet.innerText = `Balance: ${currencyAmount}$`
+
+username = prompt("What is your name?")
+document.getElementById("username").innerHTML = username
+
+
+// Get Cash
+cashButton.onclick = function(){
+  currencyAmount = currencyAmount + 100
+  wallet.innerText = `Balance: ${currencyAmount}$`
+}
+
+let betAmount = 0
+document.getElementById("submitAmount").onclick = function(){
+  betAmount = document.getElementById("depositAmount").valueAsNumber
+  document.getElementById("chosenAmount").innerText = `You are betting: ${betAmount}$`
+}
+
+
+
+// Randomizing color
+
+let coinflipResult = document.getElementById("coinflipResult")
+
+document.getElementById("submitBet").onclick = function(){
+  console.log("klicka submit")
+  if (betAmount <= currencyAmount && chosenColor != "") {
+    randomNumber = Math.floor(Math.random()*2)
+    console.log(`random number är ${randomNumber}`)
+    
+    if (randomNumber == 0) {
+      rightColor = "red"
+      coinflipResult.style.backgroundImage = "url(pokerred.png)"
+      setTimeout(resetCoinflip, 5000)      
+    }
+    else if (randomNumber == 1) {
+      rightColor = "black"
+      coinflipResult.style.backgroundImage = "url(pokerblack.png)"
+      setTimeout(resetCoinflip, 5000)
+    }
+
+    if (chosenColor == rightColor) {
+      currencyAmount += betAmount
+    }
+    else {
+      currencyAmount -= betAmount
+    }
+
+    document.getElementById("wallet").innerHTML = `Balance: ${currencyAmount}$`
+    setTimeout(resetCoinflip, 5000)
   }
-});
+}
 
-// const ChanceJS = require("chance");
-// const serverSeed = "";
-// const randomSeed = "";
-// const mod = `${serverSeed}-${randomSeed}`;
+// Choose color on coinflip
 
-// const chance = new ChanceJS(mod);
-// const ticket = chance.floating({ min: 0, max: 1, fixed: 8 });
+let chosenColor = ""
 
-// console.log(ticket);
+document.getElementById("chooseRed").onclick = function(){
+  console.log("Change red")
+  document.getElementById("chooseRed").classList.add("selected")
+  document.getElementById("chooseBlack").classList.remove("selected")
+  chosenColor = "red"
+}
+document.getElementById("chooseBlack").onclick = function(){
+  console.log("Change Black")
+  document.getElementById("chooseBlack").classList.add("selected")
+  document.getElementById("chooseRed").classList.remove("selected")
+  chosenColor = "black"
+}
+
+
+
+function resetCoinflip() {
+  document.getElementById("chosenAmount").innerText = `You are betting:`
+  chosenColor = ""
+  rightColor = ""
+  betAmount = 0
+  document.getElementById("chooseBlack").classList.remove("selected")
+  document.getElementById("chooseRed").classList.remove("selected")
+  // coinflipResult.style.backgroundImage = "linear-gradient(to right, greenyellow, red)"
+}
